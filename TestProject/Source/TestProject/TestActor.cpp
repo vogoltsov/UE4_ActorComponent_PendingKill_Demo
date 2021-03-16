a237e984b81f3ada45460c6f165ac9b520820d2c
Vitaly Ogoltsov <vitaly.ogoltsov@me.com>
2021-03-16 18:13:35 +0300

    Changed component name in ATestActor.

--- TestProject/Source/TestProject/TestActor.cpp
+++ TestProject/Source/TestProject/TestActor.cpp
@@ -11,7 +11,7 @@ ATestActor::ATestActor()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-    CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("OldComponentName"));
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("NewComponentName"));
     CapsuleComponent->SetupAttachment(RootComponent);
 }
 
